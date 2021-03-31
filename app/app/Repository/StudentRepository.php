d6b8aa4df7a77c798e1b97834a860fa315d65a86
yessaliyev <40271311+yessaliyev@users.noreply.github.com>
2021-03-31 17:50:55 +0600

    подготовил запрос

--- app/app/Repository/StudentRepository.php
+++ app/app/Repository/StudentRepository.php
@@ -1,10 +1,10 @@
 <?php
 
-
 namespace App\Repository;
 
-
+use App\Models\Group;
 use App\Models\User;
+use League\Route\Http\Exception\NotFoundException;
 
 class StudentRepository
 {
@@ -17,6 +17,29 @@ class StudentRepository
 
     public function findSuitableGroup()
     {
-        return $this->user->skills()->get();
+        $skills = $this->user->skills()->get();
+
+        if (empty($skills)) {
+            throw new NotFoundException('skills not found');
+        }
+
+        //получаю только id skill-ов
+        $skillIds = $skills->pluck('id')->toArray();
+
+        $sql = 'select * from groups where id = (
+                select id
+                from (
+                         select groups.id, count(groups.id) as counter
+                         from groups
+                                  inner join groups_skills gs on groups.id = gs.group_id
+                                  left join skills s on gs.skill_id = s.id
+                         where skill_id in (1,3,5)
+                         group by groups.id
+                         order by counter desc
+                     ) counter
+                limit 1);';
+//        $groups = Group::where([''])
+
+        return ['group'];
     }
 }
