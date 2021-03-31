f88ebfdfccd548cee559aeb718d0eb2a2296228d
Alex <mar4ehk0@gmail.com>
2021-03-31 18:28:34 +0300

    добавил константы Request

--- app/app/Models/Request.php
+++ app/app/Models/Request.php
@@ -7,6 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Request extends Model
 {
+    const OPEN = 'open';
+    const CLOSE = 'completed';
+
     protected $table = 'requests';
     protected $fillable = [
         'status',
