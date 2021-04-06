48165860e1ca7239ff16bb367a1b8c9a643a78e3
Alex <mar4ehk0@gmail.com>
2021-04-06 18:55:31 +0300

    Обновил interface Cache

--- app/app/Storage/Cache.php
+++ app/app/Storage/Cache.php
@@ -11,13 +11,22 @@ interface Cache
 {
     public function getGroup(int $groupID): ?Group;
     public function setGroup(Group $group): bool;
+    public function delGroup(int $groupID): bool;
     public function setGroupSkills(int $groupID, array $skillIDs): bool;
+    public function addSkillToGroup(int $groupID, int $skillID): bool;
+    public function delSkillFromGroup(int $groupID, int $skillID): bool;
     public function getGroupSkills(int $groupID): ?array;
+    public function delGroupSkills(int $groupID): bool;
     public function setGroupUsers(int $groupID, array $userIDs): bool;
     public function getGroupUsers(int $groupID): ?array;
+    public function addUserToGroup(int $groupID, int $userID): bool;
+    public function delUserFromGroup(int $groupID, int $skillID): bool;
+    public function delGroupUsers(int $groupID): bool;
+    public function delTeacherConditionByID(int $id): bool;
     public function setTeacherCondition(TeacherCondition $teacherCondition): bool;
     public function getTeacherConditionByID(int $id): ?TeacherCondition;
     public function getTeacherConditionByUserID(int $userID): ?TeacherCondition;
     public function setUserSkills(int $userID, array $skillIDs): bool;
     public function getUserSkills(int $userID): array;
+    public function delUserSkills(int $userID): bool;
 }
