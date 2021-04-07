5623665752129257cf48125970acb83ad7baf86c
Alex <mar4ehk0@gmail.com>
2021-04-08 01:23:26 +0300

    изменил для выполнения DDL

--- DB/DDl.sql
+++ DB/DDl.sql
@@ -11,7 +11,6 @@ CREATE TABLE public.users
     created_at timestamp   NOT NULL,
     CONSTRAINT users_pk PRIMARY KEY (id)
 );
-CREATE UNIQUE INDEX users_email_idx ON public.users USING btree (email);
 
 CREATE TABLE public."groups"
 (
@@ -29,7 +28,6 @@ CREATE TABLE public."groups"
     CONSTRAINT groups_pk PRIMARY KEY (id),
     CONSTRAINT groups_fk FOREIGN KEY (user_id) REFERENCES users (id)
 );
-CREATE UNIQUE INDEX groups_name_idx ON public.groups USING btree (name);
 
 CREATE TABLE public.skills
 (
@@ -39,7 +37,6 @@ CREATE TABLE public.skills
     created_at timestamp        NOT NULL,
     CONSTRAINT skills_pk PRIMARY KEY (id)
 );
-CREATE UNIQUE INDEX skills_name_idx ON public.skills USING btree (name);
 
 CREATE TABLE public.requests
 (
@@ -58,7 +55,6 @@ CREATE TABLE public.groups_skills (
 	CONSTRAINT groups_fk FOREIGN KEY (group_id) REFERENCES groups(id) ON DELETE CASCADE,
 	CONSTRAINT skills_fk FOREIGN KEY (skill_id) REFERENCES skills(id) ON DELETE CASCADE
 );
-CREATE INDEX groups_skills_group_id_idx ON public.groups_skills USING btree (group_id, skill_id);;
 
 CREATE TABLE public.groups_users (
 	id bigserial NOT NULL,
@@ -68,7 +64,6 @@ CREATE TABLE public.groups_users (
 	CONSTRAINT groups_fk FOREIGN KEY (group_id) REFERENCES groups(id) ON DELETE CASCADE,
 	CONSTRAINT users_fk FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
-CREATE INDEX groups_users_group_id_idx ON public.groups_users USING btree (group_id, user_id);
 
 CREATE TABLE public.teachers_conditions (
 	id bigserial NOT NULL,
@@ -76,12 +71,11 @@ CREATE TABLE public.teachers_conditions (
 	max_groups_num int2 NOT NULL,
 	min_group_size int2 NOT NULL,
 	max_group_size int2 NOT NULL,
-	updated_at date NOT NULL,
-	created_at date NOT NULL,
+	updated_at timestamp NOT NULL,
+	created_at timestamp NOT NULL,
 	CONSTRAINT teachers_conditions_pk PRIMARY KEY (id),
 	CONSTRAINT users_fk FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
-CREATE UNIQUE INDEX teachers_conditions_user_id_idx ON public.teachers_conditions USING btree (user_id);
 
 CREATE TABLE public.users_skills (
 	id bigserial NOT NULL,
@@ -91,5 +85,15 @@ CREATE TABLE public.users_skills (
 	CONSTRAINT skill_fk FOREIGN KEY (skill_id) REFERENCES skills(id) ON DELETE CASCADE,
 	CONSTRAINT user_fk FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
+
+CREATE UNIQUE INDEX users_email_idx ON public.users USING btree (email);
+CREATE UNIQUE INDEX groups_name_idx ON public.groups USING btree (name);
+CREATE UNIQUE INDEX skills_name_idx ON public.skills USING btree (name);
+CREATE INDEX groups_skills_group_id_idx ON public.groups_skills USING btree (group_id, skill_id);;
+CREATE INDEX groups_users_group_id_idx ON public.groups_users USING btree (group_id, user_id);
+CREATE UNIQUE INDEX teachers_conditions_user_id_idx ON public.teachers_conditions USING btree (user_id);
 CREATE UNIQUE INDEX users_skills_user_id_idx ON public.users_skills USING btree (user_id, skill_id);
 
+ALTER TABLE public.teachers_conditions ADD CONSTRAINT teachers_conditions_check CHECK ((min_group_size <= max_group_size));
+ALTER TABLE public."groups" ADD CONSTRAINT groups_skills_check CHECK ((min_skills_num <= max_skills_num));
+ALTER TABLE public."groups" ADD CONSTRAINT groups_students_check CHECK ((min_students_num <= max_students_num));
