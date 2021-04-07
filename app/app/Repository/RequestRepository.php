cffe4cca97bdafe15189441f3329228acf0271a2
Alex <mar4ehk0@gmail.com>
2021-04-08 00:04:39 +0300

    добавил метод closeRequest в RequestRepository

--- app/app/Repository/RequestRepository.php
+++ app/app/Repository/RequestRepository.php
@@ -3,17 +3,26 @@
 
 namespace App\Repository;
 
-
 use App\Models\Request;
 
 class RequestRepository
 {
-    public static function createRequest()
+    public static function createRequest(): Request
     {
         $request = new Request();
         $request->status = Request::OPEN;
         $request->save();
 
         return $request;
+    }
+
+    public static function closeRequest(int $id): bool
+    {
+        $request = Request::where('id', $id)->first();
+        if (empty($request)) {
+//            throw new \Exception()
+        }
+        $request->status = Request::CLOSE;
+        return $request->save();
     }
 }
