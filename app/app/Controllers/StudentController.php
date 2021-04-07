ae4685a18e4692e084fa936c4d2e38de4e90c6bb
Alex <mar4ehk0@gmail.com>
2021-04-07 22:48:29 +0300

    изменил на использование StudentRepository

--- app/app/Controllers/StudentController.php
+++ app/app/Controllers/StudentController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controllers;
 
+use App\Logger\AppLogger;
+use App\Models\User;
 use App\Queue\Jobs\Worker;
 use App\Queue\RabbitMQProducer;
 use App\Repository\GroupRepository;
@@ -16,9 +18,43 @@ use Psr\Http\Message\ServerRequestInterface;
 
 class StudentController
 {
-    public function create()
+    public function create(ServerRequestInterface $request, array $args)
     {
-        return JsonResponse::respond([],201);
+        try {
+            $data = $request->getBody()->getContents();
+            $data = json_decode($data, true);
+            if (empty($data)) {
+                throw new \Exception('JSON is wrong');
+            }
+//            // нужна валидация
+//            if (empty(validate)) {
+//                throw new \Exception('Параметры ошибочны');
+//            }
+
+            $skills = $data['skills'];
+            $data['enabled'] = false;
+            unset($data['skills']);
+            $user = new User;
+            $user->email = $data['email'];
+            $user->first_name = $data['first_name'];
+            $user->last_name = $data['last_name'];
+            $user->phone = $data['phone'];
+            $user->enabled = $data['enabled'];
+            $user->teacher = false;
+            $repo = new StudentRepository(new RedisDAO());
+            $student = $repo->create($user, $skills);
+            if (empty($student)) {
+                throw new \Exception('Student was not create');
+            }
+
+            $data = ['student_id' => $student->user->id];
+            $status = 201;
+
+        } catch (\Exception $e) {
+            $data = ['message' => $e->getMessage()];
+            $status = 422;
+        }
+        return JsonResponse::respond($data, $status);
     }
 
     public function search()
@@ -29,14 +65,57 @@ class StudentController
 
     public function update(ServerRequestInterface $request, array $args)
     {
-        $data = ['asd' => 123, 'args' => $args];
-        return JsonResponse::respond($data);
+        try {
+            $data = $request->getBody()->getContents();
+            $data = json_decode($data, true);
+            if (empty($data)) {
+                throw new \Exception('JSON is wrong');
+            }
+
+            $skills = $data['skills'];
+            $data['enabled'] = false;
+            unset($data['skills']);
+            $user = new User;
+            $user->id = $args['student_id'];
+            $user->email = $data['email'];
+            $user->first_name = $data['first_name'];
+            $user->last_name = $data['last_name'];
+            $user->phone = $data['phone'];
+            $user->enabled = $data['enabled'];
+            $user->teacher = false;
+            $repo = new StudentRepository(new RedisDAO());
+            $result = $repo->update($user, $skills);
+            $status = 201;
+            if (empty($result)) {
+                $status = 422;
+            }
+            $data = ['updated' => $result];
+        } catch (\Exception $e) {
+            $data = [
+                'message' => $e->getMessage(),
+                'updated' => false
+            ];
+            $status = 422;
+        }
+        return JsonResponse::respond($data, $status);
     }
 
     public function delete(ServerRequestInterface $request, array $args)
     {
-        $data = ['asd' => 123, 'args' => $args];
-        return JsonResponse::respond($data);
+        try {
+            $repo = new StudentRepository(new RedisDAO());
+            $result = $repo->delete($args['student_id']);
+            $data = ['deleted' => $result];
+            $status = 201;
+        } catch (\Exception $e) {
+            $data = [
+                'message' => $e->getMessage(),
+                'deleted' => false
+            ];
+            $status = 422;
+        }
+
+        return JsonResponse::respond($data, $status);
     }
 
     /**
@@ -50,7 +129,6 @@ class StudentController
     {
         (new RequestValidator($args))->validate(['user_id' => 'required|numeric']);
 
-
 //        $student = new StudentRepository($this->student);
 //        $student->findSuitableGroup();
 //        $student->addToGroup();
@@ -59,17 +137,17 @@ class StudentController
 //        if ($user->isTeacher()) {
 //            throw new BadRequestException('user is a student');
 //        }
-//
+
         $queueRequest = RequestRepository::createRequest();
 
         $data = [
             'request_id' => $queueRequest->id,
-            'data' => ['id' => $args['user_id']]
+            'id' => $args['user_id']
         ];
 
         $producer = new RabbitMQProducer();
         $producer->publish(Worker::COMMAND_FIND_GROUP_NEW_USER, $data);
 
-        return JsonResponse::respond(['result' => $queueRequest->id]);
+        return JsonResponse::respond(['id' => $queueRequest->id]);
     }
 }
