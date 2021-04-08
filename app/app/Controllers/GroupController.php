de5454d1b7492e3b1da3bd15306bae6ee034cbb3
Alex <mar4ehk0@gmail.com>
2021-04-08 13:12:10 +0300

    refactor GroupController

--- app/app/Controllers/GroupController.php
+++ app/app/Controllers/GroupController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controllers;
 
+use App\Helpers\JSONHelper;
 use App\Models\DTOs\GroupDTO;
 use App\Queue\Jobs\Worker;
 use App\Queue\RabbitMQProducer;
@@ -15,34 +16,33 @@ use App\Models\Group;
 
 class GroupController
 {
+
+    const RULES_VALIDATE = [
+        'name' => 'required|alpha_spaces',
+        'min_students_num' => 'required|numeric',
+        'max_students_num' => 'required|numeric',
+        'min_skills_num' => 'required|numeric',
+        'max_skills_num' => 'required|numeric',
+        'max_useless_skill_students' => 'required|numeric',
+        'enabled' => 'required|boolean',
+        'skills' => 'required|array',
+    ];
+
     public function create(ServerRequestInterface $request, array $args)
     {
         try {
-            $data = $request->getBody()->getContents();
-            $data = json_decode($data, true);
-            if (empty($data)) {
-                throw new \Exception('JSON is wrong');
-            }
-            // нужна валидация
-            $skills = $data['skills'];
-            $data['enabled'] = false;
-            unset($data['skills']);
-            $groupDTO = new GroupDTO(
-                $data['name'],
-                null,
-                $data['min_students_num'],
-                $data['max_students_num'],
-                $data['min_skills_num'],
-                $data['max_skills_num'],
-                $data['max_useless_skill_students'],
-                false
-            );
+            $body = $request->getBody()->getContents();
+            $this->validateCreate($body);
+
+            $body = json_decode($body, true);
+            $groupDTO = $this->prepareGroupDTO($body);
+
             $repo = new GroupRepository(new RedisDAO());
             $group = $repo->create($groupDTO);
             if (empty($group)) {
                 throw new \Exception('Group was not create');
             }
-            $result = $repo->setSkillsByGroupID($group->id, $skills);
+            $result = $repo->setSkillsByGroupID($group->id, $body['skills']);
             if (!$result) {
                 throw new \Exception('Skills was not add to group ID:', $group->id);
             }
@@ -74,39 +74,26 @@ class GroupController
 
     public function update(ServerRequestInterface $request, array $args)
     {
-
         try {
-            $data = $request->getBody()->getContents();
-            $data = json_decode($data, true);
-            if (empty($data)) {
-                throw new \Exception('JSON is wrong');
-            }
-            $skills = $data['skills'];
-            unset($data['skills']);
-            $group = new Group;
-            $group->id = $args['group_id'];
-            $group->name = $data['name'];
-            $group->min_students_num = $data['min_students_num'];
-            $group->max_students_num = $data['max_students_num'];
-            $group->min_skills_num = $data['min_skills_num'];
-            $group->max_skills_num = $data['max_skills_num'];
-            $group->max_useless_skill_students
-                = $data['max_useless_skill_students'];
-            $group->enabled = $data['enabled'];
+            $body = $request->getBody()->getContents();
+            $this->validateUpdate($body, $args);
+
+            $body = json_decode($body, true);
+            $group = $this->prepareGroup($args['group_id'], $body);
 
             $repo = new GroupRepository(new RedisDAO());
             $result = $repo->update($group);
             if (empty($result)) {
                 throw new \Exception('Group was not updated.');
             }
-            $result = $repo->setSkillsByGroupID($args['group_id'], $skills);
+            $result = $repo->setSkillsByGroupID($args['group_id'], $body['skills']);
             if (empty($result)) {
                 throw new \Exception('Skills were not updated.');
             }
             $data = ['updated' => true, 'group_id' => $args['group_id']];
             $status = 201;
-            // @TODO нужна очередь ведь изменение идет группы  и
-            // поэтому таблицы groups_users может быть изменена queueproducer->publish(
+//            // @TODO нужна очередь ведь изменение идет группы  и
+//            // поэтому таблицы groups_users может быть изменена queueproducer->publish(
         } catch (\Exception $e) {
             $data = [
                 'message' => $e->getMessage(),
@@ -120,8 +107,8 @@ class GroupController
     public function delete(ServerRequestInterface $request, array $args)
     {
         try {
+            (new RequestValidator($args))->validate(['group_id' => 'required|numeric']);
             $repo = new GroupRepository(new RedisDAO());
-            // нужна валидация
             $result = $repo->delete($args['group_id']);
             $data = ['deleted' => $result];
             $status = 201;
@@ -166,5 +153,64 @@ class GroupController
         $producer = new RabbitMQProducer();
         $producer->publish(Worker::COMMAND_REPLACE_TEACHER, $data);
         return JsonResponse::respond(['result' => $data]);
+    }
+
+    private function validateCreate(string $body): void
+    {
+        $rules = self::RULES_VALIDATE;
+        unset($rules['enabled']);
+        $this->validateBody($body, $rules);
+    }
+
+    private function validateUpdate(string $body, array $args): void
+    {
+        $this->validateArgument($args);
+        $this->validateBody($body, self::RULES_VALIDATE);
+    }
+
+    private function validateArgument(array $args)
+    {
+        $validator = new RequestValidator($args);
+        $validator->validate(['group_id' => 'required|numeric']);
+    }
+
+    private function validateBody(string $body, array $rules)
+    {
+        if (!JSONHelper::isJSON($body)) {
+            throw new \Exception('Received string is not in JSON-format.');
+        }
+        $data = json_decode($body, true);
+        $validator = new RequestValidator($data);
+        $validator->validate($rules);
+    }
+
+    private function prepareGroupDTO(array $body): GroupDTO
+    {
+        return new GroupDTO(
+            $body['name'],
+            null,
+            $body['min_students_num'],
+            $body['max_students_num'],
+            $body['min_skills_num'],
+            $body['max_skills_num'],
+            $body['max_useless_skill_students'],
+            false
+        );
+    }
+
+    private function prepareGroup(int $groupID, array $body): Group
+    {
+        $properties = array_keys(self::RULES_VALIDATE);
+
+        $group = new Group;
+        $group->id = $groupID;
+        foreach ($properties as $item) {
+            if ($item == 'skills') {
+                continue;
+            }
+            $group->{$item} = $body[$item];
+        }
+
+        return $group;
     }
 }
